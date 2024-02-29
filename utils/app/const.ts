4b88dabf783a330b7a56299984b5df1db7d910a8
redthing1 <redthing1@alt.icu>
2024-02-29 15:05:54 -0800

    use default openai api host

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -8,26 +8,25 @@ export const PREFIXPROMPT = process.env.PREFIXPROMPT || "";
 
 export const POSTPROMPT = process.env.POSTPROMPT || "";
 
-// export const OPENAI_API_HOST =
-//   process.env.OPENAI_API_HOST || 'https://api.openai.com';
-let custom_openai_api_host = process.env.OPENAI_API_HOST;
-if (typeof window !== 'undefined') {
-  // in browser, use local storage instead of environment variables
-  let localstorage_openai_api_host = localStorage.getItem('OPENAI_API_HOST');
-  if (localstorage_openai_api_host) {
-    console.log(`Using OPENAI_API_HOST from local storage: ${localstorage_openai_api_host}`);
-    custom_openai_api_host = localstorage_openai_api_host;
-  }
-}
-// default to relative path, which will use the same host that serves the frontend
-export const OPENAI_API_HOST = custom_openai_api_host || '/';
-console.log(`OPENAI_API_HOST: ${OPENAI_API_HOST}`);
+// let custom_openai_api_host = process.env.OPENAI_API_HOST;
+// if (typeof window !== 'undefined') {
+//   // in browser, use local storage instead of environment variables
+//   let localstorage_openai_api_host = localStorage.getItem('OPENAI_API_HOST');
+//   if (localstorage_openai_api_host) {
+//     console.log(`Using OPENAI_API_HOST from local storage: ${localstorage_openai_api_host}`);
+//     custom_openai_api_host = localstorage_openai_api_host;
+//   }
+// }
+// // default to relative path, which will use the same host that serves the frontend
+// export const OPENAI_API_HOST = custom_openai_api_host || '/';
+// console.log(`OPENAI_API_HOST: ${OPENAI_API_HOST}`);
+export const OPENAI_API_HOST = process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
 let LLAMACPP_COMPLETIONS_ENDPOINT = '/completion';
 let GPT_COMPLETIONS_ENDPOINT = '/v1/completions';
 export const OPENAI_API_COMPLETIONS_ENDPOINT = LLAMACPP_COMPLETIONS_ENDPOINT;
 
-export const DEFAULT_TEMPERATURE = 
+export const DEFAULT_TEMPERATURE =
   parseFloat(process.env.NEXT_PUBLIC_DEFAULT_TEMPERATURE || "1");
 
 export const OPENAI_API_TYPE =
