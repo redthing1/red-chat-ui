9c1e1a51751c0ebaac51507a13486d0e5c1b8df7
redthing1 <redthing1@alt.icu>
2024-02-29 18:57:37 -0800

    make branding nicer

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -357,7 +357,7 @@ const Home = ({
       }}
     >
       <Head>
-        <title>Chatbot UI</title>
+        <title>AI Chat</title>
         <meta name="description" content="ChatGPT but better." />
         <meta
           name="viewport"
