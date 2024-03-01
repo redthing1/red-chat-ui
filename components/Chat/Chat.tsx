ccd98d2768f0ec8f57068b9c4e6fd8baa842822b
redthing1 <redthing1@alt.icu>
2024-03-01 15:15:29 -0800

    add some margin to the conversation sticky

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -711,7 +711,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
               </>
             ) : (
               <>
-                <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 text-sm text-neutral-500 dark:border-none dark:bg-[#1b1e1f] dark:text-neutral-200">
+                <div className="sticky top-0 z-10 flex justify-center border border-b-neutral-300 bg-neutral-100 py-2 mb-6 text-sm text-neutral-500 dark:border-none dark:bg-[#1b1e1f] dark:text-neutral-200">
                   {selectedConversation?.model.name}
                   <button
                     className="ml-2 cursor-pointer hover:opacity-50"
